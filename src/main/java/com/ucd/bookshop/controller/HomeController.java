7040ce290d70f07afb2daa24e9919bd5701471f2
Luis Marron <l.marron@draftkings.com>
2025-06-15 10:01:39 +0100

    🍃 Added thymeleaf support

---
+++ src/main/java/com/ucd/bookshop/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.ucd.bookshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(Model model ) {
+         model.addAttribute("name", "Luis");
+        return "index";
+    }
+}
