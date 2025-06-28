0dab476026123f00c341b189b6390dbec6b46876
Luis Marron <l.marron@draftkings.com>
2025-06-28 12:13:14 +0100

    🍲Boilerplate code removed (used beans printing)

--- src/main/java/com/ucd/bookshop/Comp47910BookStoreApplication.java
+++ src/main/java/com/ucd/bookshop/Comp47910BookStoreApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class Comp47910BookStoreApplication {
 
@@ -17,11 +15,6 @@ public class Comp47910BookStoreApplication {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
+                System.out.println("Spring Boot bookshop application started successfully! 📖");
         }; }
 }
